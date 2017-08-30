472921bb22722fdd4bfee450213a76dff57f711f
Ega Radiegtya <radiegtya@Egas-MacBook-Pro.local>
2017-08-30 15:05:19 +0700

    5. Variables

--- js/index.js
+++ js/index.js
@@ -1,5 +1,39 @@
-/*
-document.getElementById('hello_world').innerHTML = "My Name is Ega Radiegtya";
+//String
+var myName = "My Name is Ega Radiegtya";
+var address = 'South Tangerang';
+var my_name = "Joko";
+var MY_NAME = "Joko";
 
-console.log("My Name is Ega Radiegtya");
-*/
+//Integer
+var firstNumber = 1;
+var secondNumber = 4;
+
+//Decimal
+var someDecimal = 1.4;
+
+//Boolean
+var isHandsome = true;
+
+//Array
+var fruits = ['Apple', 'Banana', "Melon"];
+
+//Object
+var car = {
+  name: "Bugatti",
+  color: 'yellow',
+  cc: 9000
+}
+
+// console.log(myName)
+// console.log(address)
+// console.log(my_name)
+// console.log(MY_NAME)
+// console.log(firstNumber)
+// console.log(secondNumber)
+// console.log(someDecimal)
+// console.log(isHandsome)
+// console.log(fruits)
+console.log(car)
+console.log(car.name)
+console.log(car.color)
+console.log(car.cc)
