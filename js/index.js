d9ed842b8347c41f71a496d45ccff943c9e1af83
Ega Radiegtya <radiegtya@Egas-MacBook-Pro.local>
2017-08-31 15:02:52 +0700

    10. String Manipulation

--- js/index.js
+++ js/index.js
@@ -1,6 +1,36 @@
-function setName(){
+//length
+var txt = "qwerty";
+document.getElementById('name').innerHTML = txt.length;
 
-  var txtName = document.getElementById('txtName');
-  document.getElementById('name').innerHTML = txtName.value;
+//indexOf
+var txt = "I am seeking for a hot girl";
+document.getElementById('name').innerHTML = txt.indexOf('hot');
 
-}
+//substr
+var txt = "Apple, Microsoft, Linux";
+document.getElementById('name').innerHTML = txt.substr(0, 5);
+
+//replace
+var txt = "Please Visit Us";
+document.getElementById('name').innerHTML = txt.replace("Us", "DumbWaysToLearn");
+
+//toUpperCase
+var txt = "Please Visit Us";
+document.getElementById('name').innerHTML = txt.toUpperCase()
+
+//toLowerCase
+var txt = "Please Visit Us";
+document.getElementById('name').innerHTML = txt.toLowerCase()
+
+//charAt
+var txt = "Please Visit Us";
+document.getElementById('name').innerHTML = txt.charAt(5)
+
+//split
+var txt = "a,b,c,d";
+var txtArray = txt.split(",");
+document.getElementById('name').innerHTML = txtArray[3];
+
+//call string like array
+var txt = "a,b,c,d";
+document.getElementById('name').innerHTML = txt[3]
